2b9c119a6b9fabe73e7f5cfc1f0f4e8a240fd398
Mihai Bogdan Iordache <55545206+bogdansys@users.noreply.github.com>
2024-08-18 23:18:29 +0200

    Update Vercel configuration and settings

--- PMBackend/settings.py
+++ PMBackend/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -161,3 +160,5 @@ LOGGING = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
